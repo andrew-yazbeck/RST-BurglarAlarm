6aeb5443d0653f3d81a39053824537a0077a3b3d
andrew-yazbeck <andrew.yazbeck@stu.ocsb.ca>
2026-01-19 12:53:53 -0500

    fixed code

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Andrew
+ * Created on: Jan 2026
+ * This program creates a burglar alarm
 */
 
-basic.showString('Hello, World!')
+// comment header 
+
+let distanceToObject: number = 0
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    // Send trigger pulse
+
+    // Read echo pulse
+    distanceToObject = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters);
+    // If something is close, sound alarm
+    if (distanceToObject > 0 && distanceToObject < 15) {
+        // buzzer ON
+        basic.showIcon(IconNames.Skull)
+        music.playTone(988, music.beat(BeatFraction.Whole))
+    } else {
+        // buzzer OFF
+        music.stopAllSounds()
+        basic.clearScreen()
+    }
+    basic.pause(100)
+})
